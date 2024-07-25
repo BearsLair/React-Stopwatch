7db25b3186aa245533ed0cd5d2165603f75a8288
Patrick Gross <bears_lair@protonmail.com>
2024-07-24 17:51:45 -0700

    counter still acting weird

--- Stopwatch/src/App.jsx
+++ Stopwatch/src/App.jsx
@@ -1,45 +1,11 @@
 import "./style.css";
 import Stopwatch from "./Stopwatch";
-import { useState, useEffect } from "react";
 
 export default function App() {
-  const [sec, setSec] = useState(0);
-  const [min, setMin] = useState(0);
-  const [hr, setHr] = useState(0);
-
-  // useEffect(() => {
-  //   Timer();
-  // });
-
-  const Timer = () => {
-    setInterval(() => {
-      setSec(sec + 1);
-    }, 1000);
-
-    if (sec === 60) {
-      setSec(0);
-      setMin(min + 1);
-    }
-
-    if (min === 60) {
-      setSec(sec === 0);
-      setMin(min === 0);
-      setHr(hr + 1);
-    }
-  };
-
-  const handleStart = () => {
-    startTimer();
-  };
-
-  // const handleStop = () => { }
-
-  // const handleReset = () = {}
-
   return (
     <>
       <div className="body">
-        <Stopwatch second={sec} minute={min} hour={hr} start={handleStart} />
+        <Stopwatch />
       </div>
     </>
   );
--- Stopwatch/src/Stopwatch.jsx
+++ Stopwatch/src/Stopwatch.jsx
@@ -1,12 +1,46 @@
-export default function Stopwatch(props) {
+import { useState, useEffect } from "react";
+
+export default function Stopwatch() {
+  const [isStarted, setIsStarted] = useState(false);
+  const [isStopped, setIsStopped] = useState(true);
+  const [sec, setSec] = useState(0);
+  const [min, setMin] = useState(0);
+  const [hr, setHr] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setSec(sec + 1);
+    }, 1000);
+
+    if (sec === 60) {
+      setSec(0);
+      setMin(min + 1);
+    }
+
+    if (min === 60) {
+      setSec(sec === 0);
+      setMin(min === 0);
+      setHr(hr + 1);
+    }
+  }, [sec, min, hr, isStarted, isStopped]);
+
+  const handleStart = () => {
+    setIsStarted(true);
+    setIsStopped(false);
+  };
+
+  // const handleStop = () => {}
+
+  // const handleReset = () = {}
+
   return (
     <>
       <div className="stopwatch">
         <h1>
-          {props.hour}:{props.minute}:{props.second}
+          {hr}:{min}:{sec}
         </h1>
         <div className="stopwatch-btns">
-          <button className="start-btn" onClick={props.start}>
+          <button className="start-btn" onClick={handleStart}>
             Start
           </button>
           <button className="stop-btn">Stop</button>
